e43dc649a6b7997578c3739dec7c264dd389938a
hassan-niang <hassanniang03@gmail.com>
2023-11-10 10:14:39 -0500

    q3 finished

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Question 1 
-function createHelloWorld (){
-  return function (){
+function createHelloWorld() {
+  return function () {
     return "hello world"
   }
 }
@@ -10,36 +10,62 @@ console.log(f(2));
 
 // Question 2 
 
-let arr1 = [5,6,7,8,9,10]
+let arr1 = [5, 6, 7, 8, 9, 10]
 
-let arr2 = arr1.map((x)=> x**2) 
+let arr2 = arr1.map((x) => x ** 2)
 console.log(arr2);
 
 //Question 3
-var debounce = function(fn, t) {
-    let timeoutId; // store the timeout ID
-  
-    return function(...args) {
-      clearTimeout(timeoutId); // clear any previous timeout
-  
-      timeoutId = setTimeout(() => {
-        fn.apply(this, args); // execute the function after the delay
-      }, t);
-    };
-  };
-  
-  const log = debounce(console.log, );
-  log('Hello'); // execution cancelled
-  log('Hello'); // execution cancelled
-  log('Hello'); // 'Hello' will be logged after a delay of 100ms   
+var debounce = function (fn, t) {
+  let timeoutId; // store the timeout ID
 
-  //Thursday Question
+  return function (...args) {
+    clearTimeout(timeoutId); // clear any previous timeout
+
+    timeoutId = setTimeout(() => {
+      fn.apply(this, args); // execute the function after the delay
+    }, t);
+  };
+};
+
+const log = debounce(console.log,);
+log('Hello'); // execution cancelled
+log('Hello'); // execution cancelled
+log('Hello'); // 'Hello' will be logged after a delay of 100ms   
+
+//Thursday Question
 
 // Question 1  
 
-function createMultiplyer (value){
-  return value * 5 
+function createMultiplyer(value) {
+  return value * 5
+}
+
+console.log(createMultiplyer(10));
+
+
+//Q3
+
+let expect = (val) => {
+
+  let toBe = (val2) => {
+    if (val === val2) {
+      return "value: true"
+    } else {
+      throw new Error("Not equal")
+    }
+  }
+
+  let notToBe = (val2) => {
+    if (val === val2) {
+      throw new Error("They are equal")
+    } else {
+      return "value: true"
+    }
+  }
+  
+  return {}
 } 
 
-console.log(createMultiplyer(10)); 
+console.log(expect(5).notToBe(5));
 
